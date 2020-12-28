2e666394a7d1fc7d554e4608837c7c2141bf9488
Mason Eastman <masoneastman555@gmail.com>
2020-12-27 23:07:31 -0600

    Added code infrastructure for honey/winning

---
+++ Assets/2-the poggers gamers/HoneyScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoneyScript : MonoBehaviour
+{
+    //editor specified sprites that represent the different amounts of honey
+    //number of phases is also set in the editor for best generalizability and reusability
+    [SerializeField] Sprite[] honeyPhases;
+
+    //easter egg array of alt sprites
+    [SerializeField] Sprite[] honeyPhasesAlt;
+
+    //multiple of spaces needed to change honey sprite
+    [SerializeField] int spacesForPhaseChange;
+
+    private SpriteRenderer spriteRenderer;
+    private int index;
+    
+    //starts honey at the initial sprite when game starts
+    void Start() {   
+        index = 0;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = honeyPhases[index];
+    }
+
+    //for each spacebar press, check if we have to update the honey's phase and do so
+    //returns true if we've reached out last phase (empty) so we can switch to new honey bottle
+    public bool ProcessAction(int numSpacesPressed) {
+        //uses preincrement for the index so we can do this all in one line with ternary operator
+        spriteRenderer.sprite = (numSpacesPressed % spacesForPhaseChange == 0) ? honeyPhases[++index] : spriteRenderer.sprite;
+        return (index == honeyPhases.Length - 1);
+    }
+    //SOMETHING IS WRONG -- HONEY BOTTLES NEED TO ALL BE CENTERED OR ELSE THEYLL IMPLICITLY MOVE AROUND WHEN THEY SHOULDNT
+
+    //switches the array of honey sprites to the easter egg one
+    public void EasterEgg() {
+        honeyPhases = honeyPhasesAlt;
+    }
+}
---
+++ Assets/2-the poggers gamers/Manage.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ThePoggersGamers {
+    public class Manage : MonoBehaviour
+    {
+        //objects and their associated scripts that this manager needs
+        [SerializeField] GameObject bee;
+        private BeeScript beeScript;
+
+        [SerializeField] GameObject honey;
+        private HoneyScript honeyScript;
+
+        //after clearing this many honey bottles, win the game if time isn't up
+        [SerializeField] int numWinHoneyBottles;
+
+        //makes sure we only activate the easter egg once
+        private bool easterEgg;
+        private int numSpacesPressed; 
+        private int numHoneyBottles;
+
+        //getting the relevant scripts from the objects so we can access them
+        void Start() {
+            beeScript = bee.GetComponent<BeeScript>();
+            honeyScript = honey.GetComponent<HoneyScript>();
+            numSpacesPressed = 0;
+        }
+
+        //each frame checks for spacebar press and/or easter egg activation
+        void Update() {
+            if(Input.GetKeyDown(KeyCode.Space)) {
+                ++numSpacesPressed;
+                //TODO: despawn spacebar object at the bottom of screen
+                beeScript.SwitchArms();
+                //honey does what it needs to on spacebar press
+                //spawnNewBottle is true if we have to switch to a new bottle of honey
+                bool spawnNewBottle = honeyScript.ProcessAction(numSpacesPressed);
+                if(spawnNewBottle) {
+                    ++numHoneyBottles;
+                    //switches honey bottles to keep game going
+                    SwitchHoney();
+                }
+                //check the win condition, update if necessary (using ternary operator)
+                //game doesn't end right away, so keep win condition true even if we've already hit number to win
+                MinigameManager.Instance.minigame.gameWin = (numHoneyBottles >= numWinHoneyBottles) ? true : false;
+            }
+            checkEasterEgg();
+        }
+
+        //despawn empty honey and spawn new honey
+        //thing at the top to show how many bottles you've eaten? Update? Or maybe just have full bottles visible one one side of table and empty bottles on the other, and update those sprites? 
+        void SwitchHoney() {
+
+        }
+
+        //OOOOOOOO SUPER SECRET EASTER EGG!!!!
+        //checks for the triggering of the easter egg and handles components correctly
+        void checkEasterEgg() {
+            if(!easterEgg && Input.GetKeyDown(KeyCode.B)) {
+                easterEgg = true;
+                beeScript.EasterEgg();
+                honeyScript.EasterEgg();
+                //background.GetComponent<SpriteRenderer>().sprite = backgroundAlt;
+                //OR backgroundScript.EasterEgg();
+            }
+        }
+    }
+}
